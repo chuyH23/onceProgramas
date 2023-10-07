db2351785b25af3d8a715099c5b724973fea4202
chuyH23 <jesus.hernandez.ruiz@uabc.edu.mx>
2023-10-06 19:28:22 -0700

    programa de combinar oraciones

---
+++ matlab/matlab.py
@@ -0,0 +1,7 @@
+sabor = input("ingresa una proteina: ")
+tortilla=input("ingresa un tipo de tortilla: ")
+ubicacion=input("un lugar: ")
+
+print("quiero unos tacos de "+sabor)
+print("de tortilla de  "+tortilla)
+print("y los comere en "+ubicacion)
---
+++ matlab/readme.md
@@ -0,0 +1,3 @@
+### juego mat lab
+
+## aqui se rempalazarn variables por cosas que hagan sentido armando oraciones
